f1bf3952e00fbcc7bc5b1ddd1f18d681bd582bef
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-11-29 23:58:14 +0900

    feat: pagination 변수 정의

--- pagination/src/hooks/usePagination.ts
+++ pagination/src/hooks/usePagination.ts
@@ -6,3 +6,31 @@ interface Props {
     siblingCount?: number;
     boundaryCount?: number;
 }
+
+const usePagination = ({count, page, onPageChange, disavled, siblingCount=1, boundaryCount=1}:Props) => {
+    const range = (start:number, end: number) => {
+        const length = end - start +1;
+
+        return Array.from({length}).map(((_,index) => index+start))
+    }
+
+    const startPage = 1;
+    const endPage = count;
+
+    const startPages = range(startPage, Math.min(boundaryCount, count));
+    const endPages = range(Math.max(count-boundaryCount+1, boundaryCount+1),count);
+
+    const siblingStart = Math.max(
+        Math.min(
+            page+1 - siblingCount,
+            count - boundaryCount - siblingCount * 2 -1
+        ),
+        boundaryCount + 2 
+    )
+    const siblingEnd = Math.min(
+        Math.max(
+            page + 1 + siblingCount,
+            boundaryCount + siblingCount * 2 + 2,
+        ),
+        endPages.length > 0 ? endPages[0] - 2 : endPage - 1 
+    )
