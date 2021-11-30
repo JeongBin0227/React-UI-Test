caf1e9eb40bc57f1d295b80d1c36cfd7ac3058cd
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-11-30 21:20:19 +0900

    feat: pagination logic

---
+++ pagination/src/components/Pagination.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import usePagination from '../hooks/usePagination'
+
+interface Props {
+    count: number;
+    page: number;
+    onPageChange: (page: number) => void;
+    disabled?: boolean;
+    siblingCount?: number;
+    boundaryCount?: number;
+}
+
+const Pagination: React.FC<Props> = ({
+    count,
+    page,
+    onPageChange,
+    disabled,
+    siblingCount,
+    boundaryCount}) => {
+        const {}  = usePagination({
+            count,
+            page,
+            onPageChange,
+            disabled,
+            siblingCount,
+            boundaryCount})
+        return(
+            <nav>
+                <li>
+                    {
+                        items.map(({key, disabled, selected, onClick, item})=>(
+                            <li key={key}>
+                                <button disabled={disabled} onClick={onClick}>{item}</button>
+                            </li>
+                        ))
+                    }
+                </li>
+            </nav>
+        )
+}
+
+export default Pagination
